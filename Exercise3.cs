f2ee219c4b708723994c0e1e155e72be0af461cd
Paul Uagbale <charlesuagbale@gmail.com>
2025-09-06 06:29:04 +0100

    Create Exercise3.cs

---
+++ Exercise3.cs
@@ -0,0 +1,12 @@
+// Exercise 3: Loops
+// This program prints numbers from 1 to 10 using a simple for loop.
+
+using System;
+
+class Program {
+    static void Main() {
+        for (int i = 1; i <= 10; i++) {
+            Console.WriteLine(i);
+        }
+    }
+}
